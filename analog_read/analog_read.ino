c57f2ea949914655ec76541c49a90ee0ab299fe5
LuMa94 <34252850+LuMa94@users.noreply.github.com>
2017-12-04 22:37:01 +0100

    Add files via upload

---
+++ analog_read/analog_read.ino
@@ -0,0 +1,16 @@
+int analogPin = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3
+                       // outside leads to ground and +5V
+int val = 0;           // variable to store the value read
+int val2=0;
+
+void setup()
+{
+  Serial.begin(9600);              //  setup serial
+}
+
+void loop()
+{
+  val = analogRead(analogPin);     // read the input pin
+  val2=(val*5)/1024 - 1,66 ;
+  Serial.println(val2);             // debug value
+}
